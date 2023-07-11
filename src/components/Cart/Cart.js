79e4ee093337f3fdd0d52dceef87fb7c1fecf794
Alessio <manyblake@gmail.com>
2023-07-11 11:40:54 +0200

    add CartProvider component

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -4,11 +4,11 @@ import classes from "./Cart.module.css";
 const Cart = (props) => {
   const cartItems = (
     <ul className={classes["cart-items"]}>
-      {[{ id: "c1", name: "Sushi", amount: "2", price: "12.99" }].map(
-        (item) => (
-          <li>{item.name}</li>
-        )
-      )}
+      {[
+        { key: "c1", id: "c1", name: "Sushi", amount: "2", price: "12.99" },
+      ].map((item) => (
+        <li>{item.name}</li>
+      ))}
     </ul>
   );
 
--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,11 +1,35 @@
+import { useRef, useState } from "react";
+
 import Input from "../../UI/Input";
 import classes from "./MealItemForm.module.css";
 
-const MealItem = (props) => {
+const MealItemForm = (props) => {
+  const amountInputRef = useRef();
+  const [amountIsValid, setAmountIsValid] = useState(true);
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const enteredAmount = amountInputRef.current.value;
+    const enteredAmountNumber = +enteredAmount;
+
+    if (
+      enteredAmount.trim().length === 0 ||
+      enteredAmountNumber < 1 ||
+      enteredAmountNumber > 5
+    ) {
+      setAmountIsValid(false);
+      return;
+    }
+
+    props.onAddToCart(enteredAmountNumber);
+  };
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <Input
         label="Amount"
+        ref={amountInputRef}
         input={{
           id: "amount_" + props.id,
           type: "number",
@@ -16,8 +40,9 @@ const MealItem = (props) => {
         }}
       />
       <button>+ Add</button>
+      {!amountIsValid && <p>Please enter a valid amount (1-5)</p>}
     </form>
   );
 };
 
-export default MealItem;
+export default MealItemForm;
